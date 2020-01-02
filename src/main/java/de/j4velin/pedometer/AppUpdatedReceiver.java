755b320a28831c4a4eb218f0d86a7a623717adc6
samerasahar <samera.sahar@bil.omu.edu.tr>
2020-01-02 09:25:56 +0300

    GH Commit

---
+++ src/main/java/de/j4velin/pedometer/AppUpdatedReceiver.java
@@ -0,0 +1,27 @@
+/*
+MobilProgramlamaOdevi_SAMERA_SAHAR
+ */
+
+package de.j4velin.pedometer;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+import de.j4velin.pedometer.util.API26Wrapper;
+import de.j4velin.pedometer.util.Logger;
+
+public class AppUpdatedReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(final Context context, final Intent intent) {
+        if (BuildConfig.DEBUG) Logger.log("app updated");
+        if (Build.VERSION.SDK_INT >= 26) {
+            API26Wrapper.startForegroundService(context, new Intent(context, SensorListener.class));
+        } else {
+            context.startService(new Intent(context, SensorListener.class));
+        }
+    }
+
+}
